0ebecd2207127ebf83a0dd31931d81597af6be76
George Plotnikov <accembler@gmail.com>
2018-01-26 01:44:12 +0300

    Add files via upload

---
+++ ImageBlendingAlgorithms/IBALib/Algorithms/MostDark.cs
@@ -0,0 +1,28 @@
+﻿using IBALib.Interfaces;
+using IBALib.Types;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IBALib.Algorithms
+{
+    [ImageBlendingAlgorithm]
+    internal class MostDark : IBlendAlgorithm
+    {
+        public string GetName()
+        {
+            return "MostDark";
+        }
+
+        public Color Calculate(IEnumerable<Color> colors)
+        {
+            var a = colors.ElementAt(0);
+            var b = colors.ElementAt(1);
+            return (a.R + a.G + a.B) / 3f >= (b.R + b.G + b.B) / 3f ? b : a;
+        }
+
+        public string GetVerboseName()
+        {
+            return "MostDark";
+        }
+    }
+}
