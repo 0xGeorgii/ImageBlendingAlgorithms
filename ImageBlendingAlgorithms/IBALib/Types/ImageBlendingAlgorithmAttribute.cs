7846a837f6601d7466a0cc2c1315e4785b4d7164
George Plotnikov <accembler@gmail.com>
2018-01-26 01:42:10 +0300

    Add files via upload

---
+++ ImageBlendingAlgorithms/IBALib/Types/ImageBlendingAlgorithmAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBALib.Types
+{
+    public class ImageBlendingAlgorithmAttribute : Attribute
+    {
+        
+    }
+}
