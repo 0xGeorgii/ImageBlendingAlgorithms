4bb0365ba7cb85fb8fad9d23cfbdded01707a85a
George Plotnikov <accembler@gmail.com>
2018-03-09 00:55:38 +0300

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,19 +1,6 @@
-function Exec  
-{
-    [CmdletBinding()]
-    param(
-        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
-        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
-    )
-    & $cmd
-    if ($lastexitcode -ne 0) {
-        throw ("Exec: " + $errorMessage)
-    }
-}
-
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
-exec { & cd ImageBlendingAlgorithms }
-exec { & dotnet restore }
-exec { & dotnet test .\IBALibTest -c Release }
+cd ImageBlendingAlgorithms
+dotnet restore
+dotnet test .\IBALibTest -c Release
 $revision = '0.0.' + $env:APPVEYOR_BUILD_NUMBER
-exec { & dotnet pack .\src\IBALib -c Release -o .\artifacts --version=$revision }
+dotnet pack .\src\IBALib -c Release -o .\artifacts --version=$revision
