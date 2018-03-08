d4aec4d3c7fed3164c48fa25ba7af58963ffcc5b
George Plotnikov <accembler@gmail.com>
2018-03-09 00:50:22 +0300

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,16 @@
+function Exec  
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 exec { & dotnet restore }
 exec { & dotnet test .\IBALibTest -c Release }
