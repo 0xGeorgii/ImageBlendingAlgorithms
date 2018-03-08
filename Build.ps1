877e77957cd49dbbd70bad39279ba2cb60f6212a
George Plotnikov <accembler@gmail.com>
2018-03-09 02:09:53 +0300

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,11 +1,22 @@
+Write-Host "Cleaning artifacts"
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+Write-Host "Done cleaning artifacts"
 
 cd ImageBlendingAlgorithms
 
+Write-Host "Restoring packages"
 nuget restore -recursive
+Write-Host "Done restoring packages"
 
-MSBuild.exe ImageBlendingAlgorithms.sln /p:Configuration=Release /p:Platform="Any CPU"
+Write-Host "Building solution"
+dotnet build ImageBlendingAlgorithms.sln
+Write-Host "Done building solution"
 
+Write-Host "Executing tests"
 dotnet test .\Tests\IBALibTest.csproj -c Release
+Write-Host "Done executing tests"
+
 $revision = $env:APPVEYOR_BUILD_NUMBER
-dotnet pack .\IBALib /p:Platform="Any CPU" -c Release -o .\artifacts /p:PackageVersion=0.0.1 --version-suffix=$revision
+Write-Host "Packaging project"
+dotnet pack .\IBALib -c Release -o .\artifacts --version-suffix=$revision
+Write-Host "Done packaging projects"
