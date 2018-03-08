26d2549535f05e11a381817cbd6c49225ceac3c5
George Plotnikov <accembler@gmail.com>
2018-03-09 01:28:34 +0300

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,34 +1,11 @@
-function EnsurePsbuildInstalled{  
-    [cmdletbinding()]
-    param(
-        [string]$psbuildInstallUri = 'https://raw.githubusercontent.com/ligershark/psbuild/master/src/GetPSBuild.ps1'
-    )
-    process{
-        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
-            'Installing psbuild from [{0}]' -f $psbuildInstallUri | Write-Verbose
-            (new-object Net.WebClient).DownloadString($psbuildInstallUri) | iex
-        }
-        else{
-            'psbuild already loaded, skipping download' | Write-Verbose
-        }
-
-        # make sure it's loaded and throw if not
-        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
-            throw ('Unable to install/load psbuild from [{0}]' -f $psbuildInstallUri)
-        }
-    }
-}
-
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
-
-EnsurePsbuildInstalled
 
 cd ImageBlendingAlgorithms
 
 dotnet restore
 
-Invoke-MSBuild ImageBlendingAlgorithms.sln /p:Configuration=Release /p:Platform="Any CPU"
+dotnet build -c Release
 
-dotnet test .\IBALibTest -c Release
+dotnet test .\Tests\IBALibTest.csproj -c Release
 $revision = '0.0.' + $env:APPVEYOR_BUILD_NUMBER
-dotnet pack .\src\IBALib -c Release -o .\artifacts --version=$revision
+dotnet pack .\IBALib -c Release -o .\artifacts --version=$revision
